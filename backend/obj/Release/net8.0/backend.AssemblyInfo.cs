b1ff11bffcd3308ee813e7164bcaf14325a7fac5
francoasevey <francoasevey@gmail.com>
2024-04-27 16:02:42 -0300

    try controller

--- backend/obj/Release/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/backend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea70b193176e598d60002395c9138758773bd87")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
